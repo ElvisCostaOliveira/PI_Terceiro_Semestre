b9c00338a758cf2a9a4da006aa905d9a50eee68a
Elvis Oliveira <elvis.oliveira@eccox.com>
2023-11-30 19:46:12 -0300

    Ajuste da pagina home

--- resources/views/promocoes.blade.php
+++ resources/views/promocoes.blade.php
@@ -1,34 +1,53 @@
 @extends('layout.app')
-    @section('main')
-    <style>
-    .card{
-        display: inline-block;
-       margin-top: 3%;
-        position: relative;
-    }
+@section('main')
 
-</style>
+</div>
+<!--Linha PRODUTOS EM DESTAQUE-->
+<section class="produtos">
+    <span class="title-secundary">Produtos em Destaque</span>
+    <hr>
+    <div class="row row-cols-1 row-cols-md-3 g-3">
+        @foreach(\App\Models\Produto::all()->take(3) as $produto)
 
-    <h1>Produtos abaixo de R$ 100,00</h1>
-
-   @foreach($produtos as $produto)
-
-
-
-<div class="card" style="width: 18rem;">
-        @if (count($produto->ProdutoImagem)>0 )
-        <img src="{{$produto->ProdutoImagem[0]->IMAGEM_URL}}" class="card-img-top" alt="...">
-        @else
-        <img src="../img/indisponivel.jpg" class="card-img-top" alt="...">
-        @endif
-        <div class="card-body">
-            <h5 class="card-title">{{$produto->PRODUTO_NOME}}</h5>
-            <!-- <p class="card-text">{{$produto->PRODUTO_DESC}}</p> -->
-            <p class="card-text">R${{$produto->PRODUTO_PRECO}}</p>
-            <a href="{{route('produto.show', $produto->PRODUTO_ID)}}"><button type="submit" class="buttonLogin">Comprar</button></a>
+        <div class="col">
+            <div class="card">
+                <img src="{{$produto->ProdutoImagem[0]->IMAGEM_URL}}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{$produto->PRODUTO_NOME}}</h5>
+                    <p class="card-text">R${{$produto->PRODUTO_PRECO}}</p>
+                    <a href="{{route('produto.show', $produto->PRODUTO_ID)}}">
+                        <button type="submit" class="buttonLogin">Comprar <i class="ri-shopping-cart-line"></i></button></a>
+                </div>
+            </div>
         </div>
-    </div>
 
+        @endforeach
 
-@endforeach
- @endsection
+        <!-- Segundo Card mais vendidos -->
+        <section class="produtos02">
+            <span class="title-secundary">Mais Vendidos</span>
+            <hr>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                @foreach(\App\Models\Produto::all()->take(6) as $produto)
+
+                <div class="col">
+                    <div class="card">
+                        @if (count($produto->ProdutoImagem)>0 )
+                        <img src="{{$produto->ProdutoImagem[0]->IMAGEM_URL}}" class="card-img-top" alt="...">
+                        @else
+                        <img src="../img/indisponivel.jpg" class="card-img-top" alt="...">
+                        @endif
+                        <div class="card-body">
+                            <h5 class="card-title">{{$produto->PRODUTO_NOME}}</h5>
+                            <p class="card-text">R${{$produto->PRODUTO_PRECO}}</p>
+                            <a href="{{route('produto.show', $produto->PRODUTO_ID)}}"><button type="submit" class="buttonLogin">Comprar <i class="ri-shopping-cart-line"></i></button></a>
+                        </div>
+                    </div>
+                </div>
+
+                @endforeach
+
+        </section>
+</section>
+@endsection
+
--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -9,24 +9,22 @@
 
 </style>
 <h1>Todos os nossos produtos</h1>
-@foreach($produtos as $produto)
-
+@foreach ($produtos as $produto)
     <div class="card" style="width: 30rem;">
-                        @if (count($produto->ProdutoImagem)>0 )
-                        <img src="{{$produto->ProdutoImagem[0]->IMAGEM_URL}}" class="card-img-top" alt="...">
-                        @else
-                        <img src="../img/indisponivel.jpg" class="card-img-top" alt="...">
-                        @endif
-                        <div class="card-body">
-                            <h5 class="card-title">{{$produto->PRODUTO_NOME}}</h5>
-                            <!-- <p class="card-text">{{$produto->PRODUTO_DESC}}</p> -->
-                            <p class="card-text">R${{$produto->PRODUTO_PRECO}}</p>
-                            <a href="{{route('produto.show', $produto->PRODUTO_ID)}}"><button type="submit" class="buttonLogin">Comprar</button></a>
-                        </div>
-                    </div>
-
-
-
-
+        @if ($produto->ProdutoImagem->isNotEmpty() && !empty($produto->ProdutoImagem[0]->IMAGEM_URL))
+            <img src="{{ $produto->ProdutoImagem[0]->IMAGEM_URL }}" class="card-img-top" alt="imagem">
+        @else
+            <img src="..\img\indisponivel.jpg" class="card-img-top" alt="...">
+        @endif
+        <div class="card-body">
+            <h5 class="card-title">{{ $produto->PRODUTO_NOME }}</h5>
+            <!-- <p class="card-text">{{ $produto->PRODUTO_DESC }}</p> -->
+            <p class="card-text">R${{ $produto->PRODUTO_PRECO }}</p>
+            <a href="{{ route('produto.show', $produto->PRODUTO_ID) }}">
+                <button type="submit" class="buttonLogin">Comprar</button>
+            </a>
+        </div>
+    </div>
 @endforeach
+
  @endsection
