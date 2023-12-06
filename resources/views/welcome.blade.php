678af4d37e21f49b3096b24cf68122420cc04c39
carlos alberto hardman junior <carloshardmanjr@hotmail.com>
2023-12-06 11:08:40 -0300

    organizei as abas de categorias

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -317,73 +317,69 @@
                             </button>
 
                             <div id="m_header_menu" class="m-header-menu m-aside-header-menu-mobile m-aside-header-menu-mobile--offcanvas m-header-menu--skin-dark m-header-menu--submenu-skin-light m-aside-header-menu-mobile--skin-light m-aside-header-menu-mobile--submenu-skin-light">
-                                <ul class="m-menu__nav m-menu__nav--submenu-arrow">
+    <ul class="m-menu__nav m-menu__nav--submenu-arrow">
+        @foreach(\App\Models\Categoria::all() as $categoria)
+            <li class="m-menu__item m-menu__item--submenu m-menu__item--rel categoria-item" m-menu-submenu-toggle="click" aria-haspopup="true">
+                <a href="{{ route('categoria.show', $categoria->CATEGORIA_ID) }}" class="m-menu__link m-menu__toggle">
+                    <span class="m-menu__item-here"></span>
+                    <span class="m-menu__link-text">{{ $categoria->CATEGORIA_NOME }}</span>
+                    <i class="m-menu__hor-arrow la la-angle-down"></i>
+                    <i class="m-menu__ver-arrow la la-angle-right"></i>
+                </a>
+                <div class="m-menu__submenu m-menu__submenu--classic m-menu__submenu--left">
+                    <span class="m-menu__arrow m-menu__arrow--adjust" style="z-index: 1;"></span>
+                    <ul class="m-menu__subnav m-scrollable" data-scrollbar-shown="true" data-scrollable="true" style="max-height:500px; overflow-y: visible;">
+                        <li class="m-menu__item" aria-haspopup="true">
+                            <a href="{{ route('categoria.show', $categoria->CATEGORIA_ID) }}" class="m-menu__link" title="{{ $categoria->CATEGORIA_NOME }}">
+                                <span class="m-menu__link-title">
+                                    <span class="m-menu__link-wrap">
+                                        <span class="m-menu__link-text">{{ $categoria->CATEGORIA_NOME }}</span>
+                                        <span class="m-menu__link-badge notranslate">
+                                            <span class="m-badge m-badge--default">
+                                                {{ $categoria->Produtos->count() }}
+                                            </span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        @endforeach
+        <li style="position:fixed; top:140px; left:1400px;"><a href="#" id="btnProximo"><img src="https://cdn-icons-png.flaticon.com/512/6364/6364375.png" width="40"></a></li>
+    </ul>
+</div>
 
-                                    @foreach(\App\Models\Categoria::all() as $categoria)
-                                    <li class="m-menu__item m-menu__item--submenu m-menu__item--rel categoria-item" m-menu-submenu-toggle="click" aria-haspopup="true">
-                                        <a href="{{ route('categoria.show', $categoria->CATEGORIA_ID) }}" class="m-menu__link m-menu__toggle">
-                                            <span class="m-menu__item-here"></span>
-                                            <span class="m-menu__link-text">{{ $categoria->CATEGORIA_NOME }}</span>
-                                            <i class="m-menu__hor-arrow la la-angle-down"></i>
-                                            <i class="m-menu__ver-arrow la la-angle-right"></i>
-                                        </a>
-                                        <div class="m-menu__submenu m-menu__submenu--classic m-menu__submenu--left">
-                                            <span class="m-menu__arrow m-menu__arrow--adjust" style="z-index: 1;"></span>
-                                            <ul class="m-menu__subnav m-scrollable" data-scrollbar-shown="true" data-scrollable="true" style="max-height:500px; overflow-y: visible;">
-                                                <li class="m-menu__item" aria-haspopup="true">
-                                                    <a href="{{ route('categoria.show', $categoria->CATEGORIA_ID) }}" class="m-menu__link" title="{{ $categoria->CATEGORIA_NOME }}">
-                                                        <span class="m-menu__link-title">
-                                                            <span class="m-menu__link-wrap">
-                                                                <span class="m-menu__link-text">{{ $categoria->CATEGORIA_NOME }}</span>
-                                                                <span class="m-menu__link-badge notranslate">
-                                                                    <span class="m-badge m-badge--default">
-                                                                        {{ $categoria->Produtos->count() }}
-                                                                    </span>
-                                                                </span>
-                                                            </span>
-                                                        </span>
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </li>
-                                    @endforeach
-
-                                </ul>
-                            </div>
-                            <a href="#" id="btnProximo">Próximo</a>
-                        </div>
-                        <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-        <script>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script>
     $(document).ready(function() {
-        // Inicialmente, mostre as primeiras 11 categorias
-        var todasAsCategorias = $('.m-menu__item');
-        var itensPorPagina = 11;
-        var ultimaCategoriaExibida = 0;
+        var todasAsCategorias = $('.categoria-item');
+        var itensPorPagina = 10;
+        var paginaAtual = 0;
 
-        // Exibe as primeiras 11 categorias
+        todasAsCategorias.hide();
         todasAsCategorias.slice(0, itensPorPagina).show();
 
-        // Manipula o clique no botão Próximo
         $('#btnProximo').on('click', function() {
-            // Esconde as categorias atuais
             todasAsCategorias.hide();
+            var indiceInicial = paginaAtual * itensPorPagina;
+            var proximasCategorias = todasAsCategorias.slice(indiceInicial, indiceInicial + itensPorPagina);
 
-            // Calcula as próximas 11 categorias a serem exibidas
-            ultimaCategoriaExibida += itensPorPagina;
-            var proximasCategorias = todasAsCategorias.slice(ultimaCategoriaExibida, ultimaCategoriaExibida + itensPorPagina);
-
-            // Se não houver mais categorias, volta ao início
             if (proximasCategorias.length === 0) {
-                ultimaCategoriaExibida = 0;
+                paginaAtual = 0;
                 proximasCategorias = todasAsCategorias.slice(0, itensPorPagina);
+            } else {
+                paginaAtual++;
             }
 
-            // Exibe as próximas 11 categorias ou menos se não houver mais categorias
             proximasCategorias.show();
         });
     });
 </script>
+
+
+
 
                     </div>
                 </div>
