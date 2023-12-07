28ba6545a2b584d612af4a8c66ed251111e1b812
Elvis Oliveira <elvis.oliveira@eccox.com>
2023-12-07 17:25:59 -0300

    meus pedidos

--- resources/views/pedido/index.blade.php
+++ resources/views/pedido/index.blade.php
@@ -1,15 +1,23 @@
 @extends('layout.app')
 @section('main')
-@foreach($pedidos as $pedido)
 
 <style>
-    .card{
-        display: inline-block;
-       margin-top: 3%;
-        position: relative;
+    .pedido-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
     }
-    #invisivel {
-        display: none;
+
+    .pedido {
+        width: 30%;
+        margin: 1%;
+    }
+
+    .card {
+        display: inline-block;
+        position: relative;
+        width: 100%;
+        margin-bottom: 20px;
     }
 
     .img-carrinho {
@@ -17,26 +25,58 @@
         width: 50px; 
     }
 
+    .produtos {
+        display: none; /* Inicialmente, os produtos estão ocultos */
+    }
+
+    .btn-expandir {
+        display: block;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
 </style>
-<p></p>
-<p></p>
-<hr size="10">
-<h1 class="fw-bold mb-0 text-black">Pedido de Nº{{$pedido->PEDIDO_ID}}</h1>
-  <h6 class="mb-0 text-muted">Data do pedido:{{$pedido->PEDIDO_DATA}} </h6>
 
-    @foreach($pedido->peditoItem as $item)
-    @if($item->ITEM_QTD> 0)
-    <div class="card" style="width: 18rem;">
-        <img src="{{$item->Produto->ProdutoImagem[0]->IMAGEM_URL}}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">{{$item->Produto->PRODUTO_NOME}}</h5>
-            <p class="card-text">Valor:R${{$item->Produto->PRODUTO_PRECO-$item->Produto->PRODUTO_DESCONTO}}</p>
-            <p class="card-text">Quantidade:{{$item->ITEM_QTD}}</p>
+<p></p>
+<p></p>
+<p>&nbsp</p>
+
+<h1 class="text-center">Meus pedidos</h1>
+<div class="pedido-container">
+    @foreach($pedidos as $pedido)
+        <div class="pedido">
+            <hr size="10">
+            <h4 class="fw-bold mb-0 text-black">Pedido de Nº{{$pedido->PEDIDO_ID}}</h4>
+            <h6 class="mb-0 text-muted">Data do pedido: {{$pedido->PEDIDO_DATA}} </h6>
+
+            <button class="btn btn-primary btn-expandir" data-target="{{ $pedido->PEDIDO_ID }}">Detalhes do pedido</button>
+
+            <div class="produtos" id="produtos-{{ $pedido->PEDIDO_ID }}">
+                @foreach($pedido->peditoItem as $item)
+                    @if($item->ITEM_QTD > 0)
+                        <div class="card" style="width: 100%;">
+                            <img src="{{ $item->Produto->ProdutoImagem[0]->IMAGEM_URL }}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $item->Produto->PRODUTO_NOME }}</h5>
+                                <p class="card-text">Valor: R${{ $item->Produto->PRODUTO_PRECO - $item->Produto->PRODUTO_DESCONTO }}</p>
+                                <p class="card-text">Quantidade: {{ $item->ITEM_QTD }}</p>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+            </div>
         </div>
-    </div>
+    @endforeach
+</div>
 
-    @endif
-      @endforeach
+<script>
+    // Adiciona um listener de evento para cada botão de expansão
+    document.querySelectorAll('.btn-expandir').forEach(function(button) {
+        button.addEventListener('click', function() {
+            var targetId = this.getAttribute('data-target');
+            var produtosDiv = document.getElementById('produtos-' + targetId);
+            produtosDiv.style.display = produtosDiv.style.display === 'none' ? 'block' : 'none';
+        });
+    });
+</script>
 
-@endforeach
 @endsection
