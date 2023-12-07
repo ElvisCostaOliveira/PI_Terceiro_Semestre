15e1e1db963e5e8c831d360a4c921d1f79b33808
carlos alberto hardman junior <carloshardmanjr@hotmail.com>
2023-12-06 23:23:34 -0300

    organizacao do css no Informacao do Usuario

--- resources/views/usuarios/show.blade.php
+++ resources/views/usuarios/show.blade.php
@@ -1,26 +1,47 @@
 @extends('layout.app')
 @section('main')
 
+<style>
+    .conteiner-principal{
+      margin-left:500px; 
+      font-size:20px;
+    }
+    .posição-titulo{
+      margin-left:80px; 
+      margin-top:100px; 
+      margin-bottom: 60px;
+    }
+    .info{
+      padding-right:50px; 
+      border:1px solid black; 
+      padding:0px 20px 0px 20px; 
+      text-align:center;
+    }
+    .voltar{
+      margin-left:200px; margin-top:20px;
+    }
+</style>
+
 <div class="principal-poy">
-  <div style="margin-left:500px; font-size:20px;">
-    <h1 style="margin-left:80px; margin-top:100px; margin-bottom: 60px;"> Informações de Usuário </h1>
+  <div class="conteiner-principal" >
+    <h1 class="posição-titulo"> Informações de Usuário </h1>
     <table id="tabelaPerfil">
-      <tr style="border:1px solid black;">
-        <th style="padding-right:50px; border:1px solid black; padding:0px 20px 0px 20px; text-align:center;">Nome de Usuário</th>
-        <th  style="padding-right:50px; border:1px solid black; padding:0px 20px 0px 20px; text-align:center;">E-mail</th>
-        <th  style="padding-right:50px; border:1px solid black; padding:0px 20px 0px 20px; text-align:center;">CPF</th>
+      <tr>
+        <th class="info" >Nome de Usuário</th>
+        <th class="info">E-mail</th>
+        <th class="info">CPF</th>
       </tr>
 
 
       <t>
-        <td style="border:1px solid black; padding:0px 20px 0px 20px;">{{$user->USUARIO_NOME}}</td>
-        <td style="border:1px solid black; padding:0px 20px 0px 20px;">{{$user->USUARIO_EMAIL}}</td>
-        <td style="border:1px solid black; padding:0px 20px 0px 20px;">{{$user->USUARIO_CPF}}</td>
+        <td class="info">{{$user->USUARIO_NOME}}</td>
+        <td class="info">{{$user->USUARIO_EMAIL}}</td>
+        <td class="info">{{$user->USUARIO_CPF}}</td>
       </tr>
 
     </table>
 
-    <div class="voltar" style="margin-left:200px; margin-top:20px;">
+    <div class="voltar">
       <a href="{{route('usuarios.edit',['user'=>$user->USUARIO_ID])}} " class="btn btn-dark" role="button">Editar</a></td>
 
       <a href="/">
