2e1cc7eb4a7212a69ba01a97981507a229d7d0b6
Anthony <anthonystapf@gmail.com>
2023-12-07 01:37:37 -0300

    edção de endereço

--- resources/views/endereco/edit.blade.php
+++ resources/views/endereco/edit.blade.php
@@ -4,6 +4,36 @@
 @if(session()->has('message'))
             {{session()->get('message')}}
         @endif
+    
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    
+    <script async="" src="../js/gtm.js"></script>
+    <script async="" src="../js/gtm_1.js"></script>
+
+    <script>
+        WebFont.load({
+            google: {
+                "families": ["Poppins:300,400,500,600,700", "Roboto:300,400,500,600,700"]
+            },
+            active: function () {
+                sessionStorage.fonts = true;
+            }
+        });
+    </script>
+
+    <script src="../js/vendors.bundle.js"></script>
+
+    <link href="/css/vendors.bundle.css" rel="stylesheet" type="text/css">
+    <link href="/css/style.bundle.css" rel="stylesheet" type="text/css">
+    <link href="/css/override.css" rel="stylesheet" type="text/css">
+    <script data-ad-client="ca-pub-3376281802197214" async="" src="../js/adsbygoogle.js"></script>
+    <script id="kdtjs" type="text/javascript" async="" src="../js/k.js"></script>
+
+    </head>
 
     <script>
 
@@ -18,8 +48,48 @@
      }
      </script>
 
+     <style>
+            .principal-yt{
+                font-family: Arial, Helvetica, sans-serif;
+            width: 30%;
+            background-color: white;
+            position:static;
+            top:0px;
+            left:0%;
+            padding: 0px;
+            border-radius: 30px;
+            color:black;
+            margin-top: 5%;
+            margin-bottom: 0;
+            text-align: center;
+            transform: translate(115%,-10%);
+        }
+
+        .container {
+            background-color: rgba(255, 255, 255, 0.9);
+            backdrop-filter: blur(40px);
+            padding: 30px;
+            width: 560px;
+            border-radius: 2%;
+            font-size: 20px;
+            box-shadow: 5px 5px 5px black;
+        }
+                
+        .button-nxy{
+            background-color: #ffd700;
+            border: none;
+            padding: 15px;
+            width: 100%;
+            border-radius: 10px;
+            color:white;
+            font-size: 15px;    
+        }
+
+     </style>
+
 
     <div class="principal-yt">
+    <h3>Endereço:</h3>
     <section>
         <table>
             <ol>
@@ -33,7 +103,7 @@
                         </div>
 
                         <div>
-                            <label for="">LOGRADOURO</label>
+                            <label for="">Logradouro</label>
                             <input class="inputLogin" type="text" name="ENDERECO_LOGRADOURO" value="{{$endereco->ENDERECO_LOGRADOURO}}" required>
                         </div>
 
